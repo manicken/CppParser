4ec4caddb94269e277179108db2838fdc16dab14
manicken <65423412+manicken@users.noreply.github.com>
2021-12-16 01:14:46 +0100

    first upload

---
+++ CppExtractor/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CppExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.rtxt = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtxtRegex = new System.Windows.Forms.RichTextBox();
+            this.rtxtInput = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(125, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "parse using CppAst";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // rtxt
+            // 
+            this.rtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxt.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxt.Location = new System.Drawing.Point(605, 108);
+            this.rtxt.Name = "rtxt";
+            this.rtxt.Size = new System.Drawing.Size(681, 330);
+            this.rtxt.TabIndex = 1;
+            this.rtxt.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "parse using regex";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtxtRegex
+            // 
+            this.rtxtRegex.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtRegex.Location = new System.Drawing.Point(12, 42);
+            this.rtxtRegex.Name = "rtxtRegex";
+            this.rtxtRegex.Size = new System.Drawing.Size(984, 60);
+            this.rtxtRegex.TabIndex = 4;
+            this.rtxtRegex.Text = resources.GetString("rtxtRegex.Text");
+            // 
+            // rtxtInput
+            // 
+            this.rtxtInput.Location = new System.Drawing.Point(15, 109);
+            this.rtxtInput.Name = "rtxtInput";
+            this.rtxtInput.Size = new System.Drawing.Size(576, 328);
+            this.rtxtInput.TabIndex = 5;
+            this.rtxtInput.Text = resources.GetString("rtxtInput.Text");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "single parse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rtxtInput);
+            this.Controls.Add(this.rtxtRegex);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtxt);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.RichTextBox rtxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtxtRegex;
+        private System.Windows.Forms.RichTextBox rtxtInput;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CppExtractor/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using CppAst;
+
+using System.Text.RegularExpressions;
+
+namespace CppExtractor
+{
+    public partial class Form1 : Form
+    {
+        string folder = @"G:\arduino-1.8.13-td1.55\hardware\teensy\avr\libraries\Audio";
+
+        string classRegex = @"(?!enum).*class\b\s\b[A-Za-z_][A-Za-z_0-9]*\b\s*($)?(|:\s*($)?(public|private|protected)\s*($)?\b[^{]*\s*)\s*($)?";
+        string fieldVisibilityRegex = @"\s*(public:|private:)";
+        string functionRegex = @"\s*(unsigned|signed)?\s*(void|int|byte|char|short|long|float|double|bool)\s+(\w+)\s*(\([^)]*\))\s*";
+
+        public Form1()
+        {
+            InitializeComponent();
+            rtxtRegex.Clear();
+            rtxtRegex.AppendText(classRegex + "\n");
+            rtxtRegex.AppendText(fieldVisibilityRegex + "\n");
+            rtxtRegex.AppendText(functionRegex);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtxt.Clear();
+            string[] files = Directory.GetFiles(folder, "*.h");
+            string regex = "";
+            for (int i = 0; i < rtxtRegex.Lines.Length; i++) {
+                regex += rtxtRegex.Lines[i];
+                if (i < rtxtRegex.Lines.Length - 1)
+                    regex += "|";
+            }
+            Regex rxFunctions = new Regex(regex);
+
+            for (int i=0; i < files.Length; i++)
+            {
+                rtxt.AppendText("********************\n");
+                rtxt.AppendText(files[i] + "\n");
+                string contents = File.ReadAllText(files[i]);
+                MatchCollection func_matches = rxFunctions.Matches(contents);
+
+                if (func_matches.Count > 0)
+                {
+                    foreach (Match match in func_matches) {
+                        string groups = "";
+                        /*if (match.Groups.Count > 1)
+                        {
+                            for (int gi = 1; gi < match.Groups.Count; gi++) {
+                                
+                                groups += "["+match.Groups[gi].Value+"]";
+                            }
+                        }*/
+                        //rtxt.AppendText("@ " + match.Index + " " + match.Value.Trim() + groups +"\n");
+                        rtxt.AppendText(match.Value.Trim() + groups + "\n");
+                    }
+                }
+                rtxt.AppendText("********************\n");
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+
+
+
+            string[] files = Directory.GetFiles(folder, "*.h");
+            CppParserOptions cpo = new CppParserOptions();
+            cpo.TargetCpu = CppTargetCpu.ARM;
+            cpo.AdditionalArguments.Add("-DLAYOUT_US_ENGLISH");
+            cpo.TargetSystem = "mfloat-abi=soft";
+            cpo.IncludeFolders.Add(@"G:\arduino-1.8.13-td1.55\hardware\teensy\avr\cores\teensy4");
+            cpo.IncludeFolders.Add(@"G:\arduino-1.8.13-td1.55\hardware\tools\arm\arm-none-eabi\include");
+            cpo.IncludeFolders.Add(@"G:\arduino-1.8.13-td1.55\hardware\tools\arm\lib\gcc\arm-none-eabi\5.4.1\include");
+            cpo.IncludeFolders.Add(@"G:\arduino-1.8.13-td1.55\hardware\tools\arm\arm-none-eabi\include\c++\5.4.1");
+            cpo.IncludeFolders.Add(@"G:\arduino-1.8.13-td1.55\hardware\tools\arm\arm-none-eabi\include\c++\5.4.1\arm-none-eabi");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                rtxt.AppendText("********************\n");
+                var compilation = CppParser.ParseFile(files[i], cpo);
+                if (compilation.HasErrors)
+                    rtxt.AppendText(files[i] + " have errors\n" + compilation.Diagnostics);
+                else
+                    rtxt.AppendText(files[i] + "\n");
+
+                foreach (var cppClass in compilation.Classes)
+                {
+                    if (!cppClass.Name.StartsWith("Audio")) continue;
+
+                    rtxt.AppendText("ClassName: " + cppClass.Name + "\n");
+
+                    foreach (var func in cppClass.Functions)
+                    {
+                        if (func.Visibility.ToString() != "Public") continue;
+                        string parameters = "";
+                        foreach (var parameter in func.Parameters)
+                            parameters += parameter;
+
+                        rtxt.AppendText(func.Name + " " + parameters + " " + func.Visibility + "\n"); //Console.WriteLine(cppClass);
+                    }
+                }
+                //foreach (var cppFunction in compilation.Functions)
+                //rtxt.AppendText(cppFunction + "\n"); //Console.WriteLine(cppClass);
+                rtxt.AppendText("********************\n");
+            }
+
+        }
+
+        class ClassObj
+        {
+            public string Name = "";
+            public List<string> functions = new List<string>();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = rtxtInput.Lines;
+            //List<ClassObj> classes = new List<ClassObj>();
+
+            //ClassObj co = new ClassObj();
+
+            List<string> result = new List<string>();
+
+            for (int li = 0; li < lines.Length; li++)
+            {
+                string line = lines[li].Trim();
+                if (line.StartsWith("class"))
+                {
+                    line = line.Substring(line.IndexOf(" : public ")+ " : public ".Length);
+                    line = line.Substring(0, line.IndexOf(","));
+                    result.Add("<br>");
+                    result.Add(line +"<br>");
+                }
+                else if (line.StartsWith("if (isTarget"))
+                {
+                    line = line.Replace("if (isTarget(msg,addressOffset,\"/", "");
+                    line = line.Replace(line.Substring(line.IndexOf("))"), line.IndexOf("//") - line.IndexOf("))") - 1), "").Replace("\"", "");
+                    result.Add(line + "<br>");
+                }
+                else if (line.StartsWith("else if (isTarget"))
+                {
+                    line = line.Replace("else if (isTarget(msg,addressOffset,\"/", "");
+                    line = line.Replace(line.Substring(line.IndexOf("))"), line.IndexOf("//") - line.IndexOf("))") - 1), "").Replace("\"", "");
+                    result.Add(line + "<br>");
+                }
+            }
+
+            rtxt.Lines = result.ToArray();
+        }
+    }
+}
